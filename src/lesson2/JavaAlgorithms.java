8f1b4cffe75f3b2806c658bde8add3594fa64021
ВашеEvgenyKuzingit config --global user.email Ваш < evgenykuzin21@gmail.com>
2019-10-19 12:59:16 +0300

    one 'normal' in lesson 2.

--- src/lesson2/JavaAlgorithms.java
+++ src/lesson2/JavaAlgorithms.java
@@ -4,7 +4,12 @@ import kotlin.NotImplementedError;
 import kotlin.Pair;
 import utils.TaskUtils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 @SuppressWarnings("unused")
@@ -33,6 +38,7 @@ public class JavaAlgorithms {
      * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
+    //НЕ РАБОТАЕТ
     static public Pair<Integer, Integer> optimizeBuyAndSell(String inputName) {
         int[] costs = TaskUtils.readIntegers(inputName, 100);
         int maxDiff = costs[1] - costs[0];
@@ -42,8 +48,8 @@ public class JavaAlgorithms {
             int second = costs[i + 1];
             if (second - first > maxDiff) {
                 maxDiff = second - first;
-                System.out.println(second+ " " + first);
-                pair = new Pair<>(i+1, i+2);
+                System.out.println(second + " " + first);
+                pair = new Pair<>(i + 1, i + 2);
             }
         }
         return pair;
@@ -113,8 +119,29 @@ public class JavaAlgorithms {
      * Если имеется несколько самых длинных общих подстрок одной длины,
      * вернуть ту из них, которая встречается раньше в строке first.
      */
-    static public String longestCommonSubstring(String firs, String second) {
-        throw new NotImplementedError();
+    static public String longestCommonSubstring(String first, String second) {
+        String result = "";
+        int i = 0;
+        int j = 0;
+        int max = 0;
+        while (i < first.length() && j < first.length()) {
+            if (!second.contains(first.substring(i, j))) {
+                while (!second.contains(first.substring(i, j)) && j < first.length()) {
+                    i++;
+                    j++;
+                }
+            } else {
+                while (second.contains(first.substring(i, j))) {
+                    int len = first.substring(i, j).length();
+                    if (len > max) {
+                        max = len;
+                        result = first.substring(i, j);
+                    }
+                    j++;
+                }
+            }
+        }
+        return result;
     }
 
     /**
@@ -157,7 +184,59 @@ public class JavaAlgorithms {
      * В файле буквы разделены пробелами, строки -- переносами строк.
      * Остальные символы ни в файле, ни в словах не допускаются.
      */
+    //НЕ РАБОТАЕТ
     static public Set<String> baldaSearcher(String inputName, Set<String> words) {
-        throw new NotImplementedError();
+        String[][] matrix = new String[1000][1000];
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(inputName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String string;
+        int size = 0;
+        try {
+            int indx = 0;
+            do {
+                string = bufferedReader.readLine();
+                if (string != null) {
+                    String[] raw = string.split(" ");
+                    for (int i = 0; i < raw.length; i++) {
+                        matrix[indx][i] = raw[i];
+                        size++;
+                    }
+                }
+                indx++;
+            } while (string != null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Set<String> result = new HashSet<>();
+        for (String word : words) {
+            StringBuilder reached = new StringBuilder();
+            if (containsWord(reached, matrix, word.split(""), 0, 0, 0)){
+                result.add(word);
+            }
+        }
+        return result;
     }
+
+    private static boolean containsWord(StringBuilder reached, String[][] matrix, String[] word, int indx, int jndx, int kndx) {
+        for (int i = indx; i < matrix.length; i++) {
+
+            for (int j = jndx; j < matrix.length; j++) {
+                if (matrix[i][j].equals(word[kndx])) {
+                    reached.append(word[kndx]);
+                    if (Arrays.equals(reached.toString().split(""), word)){
+                        return true;
+                    }
+                    return containsWord(reached, matrix, word, i, j, kndx+1);
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
